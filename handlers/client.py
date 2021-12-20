fe6fcd2e1bb0935575f924aef4f86d944529c2a8
GRIEGMAK <g.m.ak@yandex.ru>
2021-12-20 17:05:00 +0600

    Минимальное готово

--- handlers/client.py
+++ handlers/client.py
@@ -1,16 +1,14 @@
-from aiogram import types, Dispatcher, bot
-from create_bot import dp
+from aiogram import types, Dispatcher
+from create_bot import dp, bot
+from telegram import ParseMode
 
 @dp.message_handler(commands=['start'])
 async def command_start(message : types.Message):
-    # await bot.send_message(message.text)
-    
-    await message.answer("Мы рады что Вы присоединились к нам!!!", message.from_user.id)
-    await message.answer("Ссылка на оригинального бота: @Moneyplace_storage_Bot")
-    await message.answer("Переходите и пользуйтесь")
-    await message.answer("Спасибо")
-    
-    print(message.text)
+    await bot.send_message(-1001716903568, str(message.from_user.id) + ' ' + str(message.from_user.first_name) + ' ' + str(message.from_user.last_name) + ' ' + '@' + str(message.from_user.username))
+    await message.answer("Здравствуйте, я бот Олег. Чтобы подключиться, нажмите на кнопку :@Moneyplace_storage_Bot.")
+    await message.answer("Чтобы попасть в чат складчины : @moneyplaceskladchina")
+    await message.answer("")
+
     
 def register_handlers_client(dp : Dispatcher):
     dp.register_message_handler(command_start, commands=['start'])
